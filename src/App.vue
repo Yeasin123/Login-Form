9c303ee531f7170389cff104215190d06cff28db
Yeasin123 <yeasin15-1037@diu.edu.bd>
2022-01-19 15:57:48 +0600

    login form

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,52 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+   <v-container class="login_card_center">
+     <v-row>
+       <v-col  md="6" offset-md="3">
+         <v-card>
+           <v-card-title class="">
+             <h3 >User Login</h3>
+           </v-card-title>
+           <v-card-text>
+             <v-text-field
+              label="User Name"
+              prepend-icon="mdi-account-circle"
+            >
+            </v-text-field>
+             <v-text-field
+              label="Password"
+              :type="showPassword ? 'text' : 'password' "
+              prepend-icon="mdi-lock"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off' "
+              @click:append="showPassword = !showPassword "
+            >
+            </v-text-field>
+           </v-card-text>
+           <v-divider></v-divider>
+           <v-card-actions>
+             <v-btn class="primary">Login</v-btn>
+           </v-card-actions>
+         </v-card>
+       </v-col>
+     </v-row>
+   </v-container>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: "App",
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  data: () => ({
+      showPassword:false,
+  }),
+};
+</script>
+<style scoped>
+.login_card_center{
+  position: relative;
+  top:50%;
+  left:50%;
+  transform: translate(-50%,-50%);
 }
 </style>
